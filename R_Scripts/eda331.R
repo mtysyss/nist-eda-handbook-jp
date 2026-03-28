2dd914353b6083ea366ccb5ce13ea375338c0457
MATSUYOSHI Yasushi <mtysyss@dd.iij4u.or.jp>
2026-03-28 16:25:39 +0900

    1st

---
+++ R_Scripts/eda331.R
@@ -0,0 +1,42 @@
+# NIST EDA 1331 Autocorreleation Plot
+# page URL: 
+#   https://www.itl.nist.gov/div898/handbook/eda/section3/eda331.htm
+#   https://www.itl.nist.gov/div898/handbook/eda/section3/autocopl.htm
+# Data URL:
+#   https://www.itl.nist.gov/div898/handbook/datasets/FLICKER.DAT
+
+# 説明 #################################################################
+# NIST Statistic Handbook 1.3.3.1の自己相関プロット
+# R標準ライブラリでのスクリプトをベースに描画調整しています。調整部分は
+# "#<-で注意書きを記しています"
+# テキストは信頼区間線分は2種類設定されていますが、R標準ライブラリでの
+# デフォルト設置は１種類です。２種類線分を描く場合には別途追加する必要
+# があります。
+
+# デフォルト環境の保存と調整############################################
+original_par <- par(no.readonly = TRUE)
+par(mar = c(3,3,2.5,1), # プロットエリア外周余白の調整
+    mgp = c(1.8,0.5,0.0), # 軸ラベル、目盛ラベル、目盛描画位置の調整
+    las = 1, # 目盛ラベル記載向きを水平に変更
+    tck = 0.02) # 目盛線を内向きに変更
+
+# データの読み込み######################################################
+url <- "https://www.itl.nist.gov/div898/handbook/datasets/FLICKER.DAT"
+data <- read.table(url,skip=25)
+
+# 自己相関プロットの作成################################################ 
+# lag.max = 250 でラグの範囲を指定
+# main, xlab, ylab でラベルを画像に合わせる
+acf(data$V1, 
+    lag.max = 250, 
+    main = "", #<- タイトル位置を近づけるために別関数で設定しています
+    xlab = "ラグ", 
+    ylab = "自己相関",
+    col = "deepskyblue", #<- メインプロットの色設定
+    ci = 0.95, #<- 信頼区間設定
+    ci.col = "red" #<- 信頼区間線文の色設置絵
+    )
+
+title(main = "自己相関プロット", line = 1) #<- タイトル位置修正
+
+on.exit(par(original_par))
