686ce36a6bae007efdd9af85d715fa5bf8bcec3e
MATSUYOSHI Y <matsuyoshiyasushi@pccpmdev.jp>
2026-04-06 14:01:00 +0900

    260406

--- R_Scripts/eda33d.R
+++ R_Scripts/eda33d.R
@@ -1,14 +1,15 @@
-# NIST EDA 1.3.3.12.DOE Mean Plot####
+# NIST EDA 1.3.3.13.DOE Standard Deviation Plot####
 # Date: 20260406
 # Created by MATSUYOSHI Yasushi PCCPMDEV.JP
 # Source: 
 # page URL: 
-#   https://www.itl.nist.gov/div898/handbook/eda/section3/eda33c.htm
-#   https://www.itl.nist.gov/div898/handbook/eda/section3/dexmeanp.htm
+#   https://www.itl.nist.gov/div898/handbook/eda/section3/eda33d.htm
+#   https://www.itl.nist.gov/div898/handbook/eda/section3/dexsdplo.htm
 # Data URL:
 #   https://www.itl.nist.gov/div898/handbook/datasets/BOXBIKE2.DAT
 # 説明 ####
-# NIST Statistic Handbook 1.3.3.12.DOE 平均プロット
+# NIST Statistic Handbook 1.3.3.13.DOE 標準偏差プロット
+# 1.3.3.12 のDOE平均プロットのmeanをsdに、ラベルを平均を標準偏差に置き換えた
 # デフォルト環境の保存と調整 ####
 original_par <- par(no.readonly = TRUE)
 par(mar = c(3,3,2,1), 
@@ -28,20 +29,20 @@ long_data <- reshape(data,
                      timevar = "Factor", 
                      times = colnames(data)[2:8])
 
-# 平均値を計算####
-mean_data <- aggregate(Y ~ Level + Factor, data = long_data, mean)
-grand_mean <- mean(long_data$Y)
+# 標準偏差を計算####
+sd_data <- aggregate(Y ~ Level + Factor, data = long_data, sd)
+grand_sd <- sd(long_data$Y)
 
 # 枠組み描画####
-plot(NULL, xlim=c(0.5, 7.5), ylim=range(mean_data$Y), 
-     xaxt="n", xlab="因子", ylab="感度", main="DOE 平均プロット")
+plot(NULL, xlim=c(0.5, 7.5), ylim=range(sd_data$Y), 
+     xaxt="n", xlab="因子", ylab="標準偏差", main="DOE 標準偏差プロット")
 #rect(par("usr")[1], par("usr")[3], par("usr")[2], par("usr")[4], col = "lightyellow") # 背景色
-abline(h = grand_mean, lty = 2, col = "red") # 総平均
+abline(h = grand_sd, lty = 2, col = "red") # 総平均
 
 # 因子ごとに線と点を描画####
-factors <- unique(mean_data$Factor)
+factors <- unique(sd_data$Factor)
 for(i in 1:length(factors)){
-  sub <- mean_data[mean_data$Factor == factors[i], ]
+  sub <- sd_data[sd_data$Factor == factors[i], ]
   points(i + sub$Level*0.2, sub$Y, pch=16, col = "blue") # 少し横にずらして描画
   lines(i + sub$Level*0.2, sub$Y, col = "blue")
 }
